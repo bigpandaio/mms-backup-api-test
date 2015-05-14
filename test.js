7e821eaabb70eb5ce668e89c0cb41dc83ee13355
Shahar Kedar <shahar@gmail.com>
2015-05-14 15:21:36 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,41 @@
+#!/usr/local/bin/node
+var request = require('request');
+var util = require('util')
+
+var groupId = null // TODO: fill in
+var clusterId = null // TODO: fill in
+var username = null // TODO: fill in
+var apiToken = null //TODO: fill in
+
+var organizations = ["bigpanda"]
+
+var excludedNamespaces = organizations.map(function(org) { return "bigpanda-data-api." + org + "_events"});
+console.log("Excluded Namespaces: " + excludedNamespaces);
+
+
+var payload = { excludedNamespaces: excludedNamespaces}
+console.log("Payload: " + util.inspect(payload));
+
+request.patch('https://mms.mongodb.com/api/public/v1.0/groups/' + groupId + '/backupConfigs/' + clusterId, {json: payload }, handleResponse)
+    .auth(username, apiToken, false);
+
+
+function handleResponse(error, response, body) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response.statusCode >= 300) {
+    console.error("HTTP Code: " + response.statusCode);
+    console.error("Body: " + util.inspect(body));
+    process.exit(1)
+  }
+
+  if (response.statusCode >= 200 && response.statusCode < 300) {
+    console.log("HTTP Code: " + response.statusCode);
+    console.log("Body:\n" + util.inspect(body));
+    process.exit(0);
+  }
+}
+
